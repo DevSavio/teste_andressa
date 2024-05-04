b1818cf1d86cfda1d9f2efea976ad4a8877c8ff3
DevSavio <develop.savio@gmail.com>
2024-05-04 20:47:22 -0300

    Retirado partes sem necessidades

--- lib/main.dart
+++ lib/main.dart
@@ -102,6 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
+      
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -139,11 +140,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
